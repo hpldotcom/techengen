e8d104e071722b5da95684055e58754753df7ef5
awad65163-gif <awad65163@gmail.com>
2026-05-08 22:04:03 +0300

    video scale

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Footer.module.css';
-import logo from '../../assets/images/Logo-t-online.png';
+import logo from '../../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
